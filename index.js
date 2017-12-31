dd678fc33bb3a147821a005fbee567a650ad1683
kumavis <aaron@kumavis.me>
2017-12-31 12:44:11 -0800

    rpc based token balance

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const EthQuery = require('eth-query')
+const Eth = require('ethjs-query')
+const EthContract = require('ethjs-contract')
+const tokenAbi = require('human-standard-token-abi')
 const HttpProvider = require('ethjs-provider-http')
 const async = require('async')
 const ethUtil = require('ethereumjs-util')
@@ -13,56 +16,60 @@ const assets = {
     digits: 18,
     coinmarketcapName: 'ethereum',
   },
-  'SwarmCity': {
-    address: '0xb9e7f8568e08d5659f5d29c4997173d84cdf2607',
-    digits: 18,
-    coinmarketcapName: 'swarm-city',
-  },
-  'Pluton': {
-    address: '0xD8912C10681D8B21Fd3742244f44658dBA12264E',
-    digits: 18,
-    coinmarketcapName: 'pluton',
-  },
-  'DGD': {
-    address: '0xe0b7927c4af23765cb51314a0e0521a9645f0e2a',
-    digits: 18,
-    coinmarketcapName: 'digixdao',
-  },
-  'MKR': {
-    address: '0xc66ea802717bfb9833400264dd12c2bceaa34a6d',
-    digits: 18,
-    coinmarketcapName: 'maker',
-  },
-  'Golem': {
-    address: '0xa74476443119A942dE498590Fe1f2454d7D4aC0d',
-    digits: 18,
-    coinmarketcapName: 'golem-network-tokens',
-  },
-  'Augur REP': {
-    address: '0x48c80F1f4D53D5951e5D5438B54Cba84f29F32a5',
-    digits: 18,
-    coinmarketcapName: 'augur',
-  },
-  'OmiseGo': {
-    address: '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07',
-    digits: 18,
-    coinmarketcapName: 'omisego',
-  },
-  'Storj': {
-    address: '0xb64ef51c888972c908cfacf59b47c1afbc0ab8ac',
-    digits: 8,
-    coinmarketcapName: 'storj',
-  },
-  'RChain RHOC': {
-    address: '0x168296bb09e24a88805cb9c33356536b980d3fc5',
-    digits: 8,
-    coinmarketcapName: 'rchain',
-  },
+  // 'SwarmCity': {
+  //   address: '0xb9e7f8568e08d5659f5d29c4997173d84cdf2607',
+  //   digits: 18,
+  //   coinmarketcapName: 'swarm-city',
+  // },
+  // 'Pluton': {
+  //   address: '0xD8912C10681D8B21Fd3742244f44658dBA12264E',
+  //   digits: 18,
+  //   coinmarketcapName: 'pluton',
+  // },
+  // 'DGD': {
+  //   address: '0xe0b7927c4af23765cb51314a0e0521a9645f0e2a',
+  //   digits: 18,
+  //   coinmarketcapName: 'digixdao',
+  // },
+  // 'MKR': {
+  //   address: '0xc66ea802717bfb9833400264dd12c2bceaa34a6d',
+  //   digits: 18,
+  //   coinmarketcapName: 'maker',
+  // },
+  // 'Golem': {
+  //   address: '0xa74476443119A942dE498590Fe1f2454d7D4aC0d',
+  //   digits: 18,
+  //   coinmarketcapName: 'golem-network-tokens',
+  // },
+  // 'Augur REP': {
+  //   address: '0x48c80F1f4D53D5951e5D5438B54Cba84f29F32a5',
+  //   digits: 18,
+  //   coinmarketcapName: 'augur',
+  // },
+  // 'OmiseGo': {
+  //   address: '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07',
+  //   digits: 18,
+  //   coinmarketcapName: 'omisego',
+  // },
+  // 'Storj': {
+  //   address: '0xb64ef51c888972c908cfacf59b47c1afbc0ab8ac',
+  //   digits: 8,
+  //   coinmarketcapName: 'storj',
+  // },
+  // 'RChain RHOC': {
+  //   address: '0x168296bb09e24a88805cb9c33356536b980d3fc5',
+  //   digits: 8,
+  //   coinmarketcapName: 'rchain',
+  // },
 }
 
 let provider = new HttpProvider('https://mainnet.infura.io/')
 const query = new EthQuery(provider)
 bindFns(query)
+
+const eth = new Eth(provider)
+const contract = new EthContract(eth)
+const TokenContract = contract(tokenAbi)
 
 async.parallel({
   accountData: (cb) => async.mapValues(accounts, calculateAssets, cb),
@@ -96,6 +103,7 @@ function showResults(err, { accountData, assetExchangeRates }) {
     })
   })
 
+  console.log('total usd:', totalUsd.toFixed(2))
   console.log('total usd:', numberWithCommas(totalUsd.toFixed(2)))
 }
 
@@ -141,18 +149,43 @@ function getEtherBalance(accountAddress, cb) {
 }
 
 
-function getTokenBalance(asset, accountAddress, cb) {
-  request(`https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=${asset.address}&address=${accountAddress}&tag=latest`, (err, res, body) => {
-    if (err) return cb(err)
-    let realTokenBalance
-    try {
-      const rawTokenBalance = parseInt(JSON.parse(body).result)
-      realTokenBalance = rawTokenBalance / Math.pow(10, asset.digits)
-    } catch (err) {
-      return cb(err)
-    }
-    cb(null, realTokenBalance)
-  })
+async function getTokenBalance(asset, accountAddress) {
+  const contract = TokenContract.at(asset.address)
+
+  const [ balanceResult, decimalsResult ] = await Promise.all([
+    contract.balanceOf(accountAddress),
+    contract.decimals(),
+  ])
+
+  const balance = balanceResult[0]
+  const decimals = decimalsResult[0]
+
+  // const ten = new BN(10)
+  // const realBalance = balance.div(ten.pow(decimals))
+  const realTokenBalance = toNumber(balance) / Math.pow(10, toNumber(decimals))
+  // console.log({ realTokenBalance, asset, decimals })
+  return realTokenBalance
+
+  function toNumber(bn){
+    return parseInt(bn.toString(16), 16)
+  }
+
+  // const [ symbol, decimals ] = results
+  // if (symbol && decimals) {
+  //   console.log('SETTING SYMBOL AND DECIMALS', { symbol, decimals })
+  //   this.setState({ symbol: symbol[0], decimals: decimals[0].toString() })
+  // }
+  // request(`https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=${asset.address}&address=${accountAddress}&tag=latest&apikey=YourApiKeyToken`, (err, res, body) => {
+  //   if (err) return cb(err)
+  //   let realTokenBalance
+  //   try {
+  //     const rawTokenBalance = parseInt(JSON.parse(body).result)
+  //     realTokenBalance = rawTokenBalance / Math.pow(10, asset.digits)
+  //   } catch (err) {
+  //     return cb(err)
+  //   }
+  //   cb(null, realTokenBalance)
+  // })
 }
 
 function bindFns(obj){
